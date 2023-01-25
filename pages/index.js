1fcf8b1df6b3c85bfd1e5ee023e979976c1c36f7
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-24 21:56:26 -0500

    Update index.js
    healing.jpg

--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,13 @@ export default function Home() {
         </h1>
         <div className={styles.hero}>
        <Image 
-                                src="/pexels-photo-4200823.webp"
+                                src="/healing.jpg"
                                 alt="Jesus teaching"
                                 height={900}
                                 width={2000}
                                 priority
                               />
-                          
+                 <div className={styles.nav}>         
    <a href="https://the-holy-bible.vercel.app/">
             <button className={styles.button}> HOME </button>
        </a>
@@ -39,6 +39,7 @@ export default function Home() {
         <a href="https://second-thessalonians.vercel.app/">
             <button className={styles.button}>NEXT > </button>
        </a>           
+            </div>
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2>Chapter 1 &darr;</h2>
