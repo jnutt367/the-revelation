cf6a2fa76306c21a4f3dd1840ee6167907a81281
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-09-30 17:01:35 -0400

    Update index.js
    <iframe width="100%" height="315" src="https://www.youtube.com/embed/HPGShWZ4Jvk?si=kz_uXOqmquqVkocd?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>

--- pages/index.js
+++ pages/index.js
@@ -6666,7 +6666,7 @@ he will bring down his anger with fury,
                </div>
   
                 <div className={styles.iframe}>
-                      <iframe width="100%" height="315" src="https://www.youtube.com/embed/5nvVVcYD-0w?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                     <iframe width="100%" height="315" src="https://www.youtube.com/embed/RSK36cHbrk0?si=jDx7bLgv07xyL2eX?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div> 
  
       <div className={styles.grid}>
@@ -6869,8 +6869,198 @@ he will bring down his anger with fury,
 
 
 
+ <main id="jeremiah" className={styles.main}>
+      
+       
+       
+        <div className={styles.hero}>
+           <h2 className={styles.holy}>THE WORD</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-holy-bible.vercel.app/" target="_blank">OF GOD</a>
+                </h1>
+        
+        <h1 className={styles.title}>
+          MALACHI
+        </h1>
+         <div  className={styles.image}>
+       <Image 
+                                src="/malachi.png"
+                                alt="image of the revealtion of jesus to john"
+                                height={900}
+                                width={2000}
+                                priority
+                              />
+        </div>
+<p className={styles.parables}>
+      Malachi is the last book in the Old testament before Mattthew & Mark, fascinating to find out why....                                
+</p>
+                      <div className={styles.nav}>           
+      <a href="https://galatians.vercel.app/#first_peter">
+             <Image 
+                                src="/previous.webp"
+                                alt="left"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+       </a>
+
+         <a href="#PayPalButton">
+            <Image 
+                                src="/upward_pointing.webp"
+                                alt="home"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         <a href="https://the-word-of-god.vercel.app/">
+           <Image 
+                                src="/next.webp"
+                                alt="right"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
+  
+                <div className={styles.iframe}>
+                     <iframe width="100%" height="315" src="https://www.youtube.com/embed/HPGShWZ4Jvk?si=kz_uXOqmquqVkocd?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div> 
+ 
+      <div className={styles.grid}>
+          <div className={styles.card}>
+            <h2 className={styles.chapter}>Malachi Chpt I</h2>
+            <p>
+1 A prophecy: The word of the Lord to Israel through Malachi.[a]
+
+Israel Doubts God’s Love
+2 “I have loved you,” says the Lord.
+
+“But you ask, ‘How have you loved us?’
+
+“Was not Esau Jacob’s brother?” declares the Lord. “Yet I have loved Jacob, 3 but Esau I have hated, and I have turned his hill country into a wasteland and left his inheritance to the desert jackals.”
+
+4 Edom may say, “Though we have been crushed, we will rebuild the ruins.”
+
+But this is what the Lord Almighty says: “They may build, but I will demolish. They will be called the Wicked Land, a people always under the wrath of the Lord. 5 You will see it with your own eyes and say, ‘Great is the Lord—even beyond the borders of Israel!’
+
+Breaking Covenant Through Blemished Sacrifices
+6 “A son honors his father, and a slave his master. If I am a father, where is the honor due me? If I am a master, where is the respect due me?” says the Lord Almighty.
+
+“It is you priests who show contempt for my name.
+
+“But you ask, ‘How have we shown contempt for your name?’
+
+7 “By offering defiled food on my altar.
+
+“But you ask, ‘How have we defiled you?’
+
+“By saying that the Lord’s table is contemptible. 8 When you offer blind animals for sacrifice, is that not wrong? When you sacrifice lame or diseased animals, is that not wrong? Try offering them to your governor! Would he be pleased with you? Would he accept you?” says the Lord Almighty.
+
+9 “Now plead with God to be gracious to us. With such offerings from your hands, will he accept you?”—says the Lord Almighty.
+
+10 “Oh, that one of you would shut the temple doors, so that you would not light useless fires on my altar! I am not pleased with you,” says the Lord Almighty, “and I will accept no offering from your hands. 11 My name will be great among the nations, from where the sun rises to where it sets. In every place incense and pure offerings will be brought to me, because my name will be great among the nations,” says the Lord Almighty.
+
+12 “But you profane it by saying, ‘The Lord’s table is defiled,’ and, ‘Its food is contemptible.’ 13 And you say, ‘What a burden!’ and you sniff at it contemptuously,” says the Lord Almighty.
+
+“When you bring injured, lame or diseased animals and offer them as sacrifices, should I accept them from your hands?” says the Lord. 14 “Cursed is the cheat who has an acceptable male in his flock and vows to give it, but then sacrifices a blemished animal to the Lord. For I am a great king,” says the Lord Almighty, “and my name is to be feared among the nations.
+
+                                  
+                                  </p>
+              </div>
+
+          <div className={styles.card}>
+           <h2 className={styles.chapter}>Malachi Chpt II</h2>
+            <p>
+  Additional Warning to the Priests
+2 “And now, you priests, this warning is for you. 2 If you do not listen, and if you do not resolve to honor my name,” says the Lord Almighty, “I will send a curse on you, and I will curse your blessings. Yes, I have already cursed them, because you have not resolved to honor me.
+
+3 “Because of you I will rebuke your descendants[a]; I will smear on your faces the dung from your festival sacrifices, and you will be carried off with it. 4 And you will know that I have sent you this warning so that my covenant with Levi may continue,” says the Lord Almighty. 5 “My covenant was with him, a covenant of life and peace, and I gave them to him; this called for reverence and he revered me and stood in awe of my name. 6 True instruction was in his mouth and nothing false was found on his lips. He walked with me in peace and uprightness, and turned many from sin.
+
+7 “For the lips of a priest ought to preserve knowledge, because he is the messenger of the Lord Almighty and people seek instruction from his mouth. 8 But you have turned from the way and by your teaching have caused many to stumble; you have violated the covenant with Levi,” says the Lord Almighty. 9 “So I have caused you to be despised and humiliated before all the people, because you have not followed my ways but have shown partiality in matters of the law.”
+
+Breaking Covenant Through Divorce
+10 Do we not all have one Father[b]? Did not one God create us? Why do we profane the covenant of our ancestors by being unfaithful to one another?
+
+11 Judah has been unfaithful. A detestable thing has been committed in Israel and in Jerusalem: Judah has desecrated the sanctuary the Lord loves by marrying women who worship a foreign god. 12 As for the man who does this, whoever he may be, may the Lord remove him from the tents of Jacob[c]—even though he brings an offering to the Lord Almighty.
+
+13 Another thing you do: You flood the Lord’s altar with tears. You weep and wail because he no longer looks with favor on your offerings or accepts them with pleasure from your hands. 14 You ask, “Why?” It is because the Lord is the witness between you and the wife of your youth. You have been unfaithful to her, though she is your partner, the wife of your marriage covenant.
+
+15 Has not the one God made you? You belong to him in body and spirit. And what does the one God seek? Godly offspring.[d] So be on your guard, and do not be unfaithful to the wife of your youth.
+
+16 “The man who hates and divorces his wife,” says the Lord, the God of Israel, “does violence to the one he should protect,”[e] says the Lord Almighty.
+
+So be on your guard, and do not be unfaithful.
+
+Breaking Covenant Through Injustice
+17 You have wearied the Lord with your words.
+
+“How have we wearied him?” you ask.
+
+By saying, “All who do evil are good in the eyes of the Lord, and he is pleased with them” or “Where is the God of justice?”
+
+            </p>
+          </div>
+
+          <div
+            className={styles.card}
+          >
+           <h2 className={styles.chapter}>Malachi Chpt III</h2>
+            <p>
+ 
+              3 “I will send my messenger, who will prepare the way before me. Then suddenly the Lord you are seeking will come to his temple; the messenger of the covenant, whom you desire, will come,” says the Lord Almighty.
+
+2 But who can endure the day of his coming? Who can stand when he appears? For he will be like a refiner’s fire or a launderer’s soap. 3 He will sit as a refiner and purifier of silver; he will purify the Levites and refine them like gold and silver. Then the Lord will have men who will bring offerings in righteousness, 4 and the offerings of Judah and Jerusalem will be acceptable to the Lord, as in days gone by, as in former years.
+
+5 “So I will come to put you on trial. I will be quick to testify against sorcerers, adulterers and perjurers, against those who defraud laborers of their wages, who oppress the widows and the fatherless, and deprive the foreigners among you of justice, but do not fear me,” says the Lord Almighty.
+
+Breaking Covenant by Withholding Tithes
+6 “I the Lord do not change. So you, the descendants of Jacob, are not destroyed. 7 Ever since the time of your ancestors you have turned away from my decrees and have not kept them. Return to me, and I will return to you,” says the Lord Almighty.
+
+“But you ask, ‘How are we to return?’
+
+8 “Will a mere mortal rob God? Yet you rob me.
+
+“But you ask, ‘How are we robbing you?’
+
+“In tithes and offerings. 9 You are under a curse—your whole nation—because you are robbing me. 10 Bring the whole tithe into the storehouse, that there may be food in my house. Test me in this,” says the Lord Almighty, “and see if I will not throw open the floodgates of heaven and pour out so much blessing that there will not be room enough to store it. 11 I will prevent pests from devouring your crops, and the vines in your fields will not drop their fruit before it is ripe,” says the Lord Almighty. 12 “Then all the nations will call you blessed, for yours will be a delightful land,” says the Lord Almighty.
+
+Israel Speaks Arrogantly Against God
+13 “You have spoken arrogantly against me,” says the Lord.
+
+“Yet you ask, ‘What have we said against you?’
+
+14 “You have said, ‘It is futile to serve God. What do we gain by carrying out his requirements and going about like mourners before the Lord Almighty? 15 But now we call the arrogant blessed. Certainly evildoers prosper, and even when they put God to the test, they get away with it.’”
+
+The Faithful Remnant
+16 Then those who feared the Lord talked with each other, and the Lord listened and heard. A scroll of remembrance was written in his presence concerning those who feared the Lord and honored his name.
+
+17 “On the day when I act,” says the Lord Almighty, “they will be my treasured possession. I will spare them, just as a father has compassion and spares his son who serves him. 18 And you will again see the distinction between the righteous and the wicked, between those who serve God and those who do not.              </p>
+          </div>
+<div className={styles.card}>
+
+<h2 className={styles.chapter}>Malachi Chpt IV</h2>
+<p>
+Judgment and Covenant Renewal
+4 [a]“Surely the day is coming; it will burn like a furnace. All the arrogant and every evildoer will be stubble, and the day that is coming will set them on fire,” says the Lord Almighty. “Not a root or a branch will be left to them. 2 But for you who revere my name, the sun of righteousness will rise with healing in its rays. And you will go out and frolic like well-fed calves. 3 Then you will trample on the wicked; they will be ashes under the soles of your feet on the day when I act,” says the Lord Almighty.
+
+4 “Remember the law of my servant Moses, the decrees and laws I gave him at Horeb for all Israel.
+
+5 “See, I will send the prophet Elijah to you before that great and dreadful day of the Lord comes. 6 He will turn the hearts of the parents to their children, and the hearts of the children to their parents; or else I will come and strike the land with total destruction.”
+</p>
+</div>
 
 
+                          
+                            </div>
+                      </div>
+              
+      </main>
 
 
 
