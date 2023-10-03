0290698722b27de1ef87d24fe9a9ca77462266cc
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-10-03 17:25:39 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6602,6 +6602,384 @@ he will bring down his anger with fury,
       </main>
 
 
+ <main id="zephaniah" className={styles.main}>
+      
+       
+       
+        <div className={styles.hero}>
+           <h2 className={styles.holy}>THE WORD</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-holy-bible.vercel.app/" target="_blank">OF GOD</a>
+                </h1>
+        
+        <h1 className={styles.title}>
+          ZEPHANIAH
+        </h1>
+         <div  className={styles.image}>
+       <Image 
+                                src="/zephaniah.png"
+                                alt="haggai image"
+                                height={900}
+                                width={2000}
+                                priority
+                              />
+        </div>
+<p className={styles.parables}>
+      The Lord their God will care for them;
+    he will restore their fortunes.                              
+</p>
+                      <div className={styles.nav6}>           
+      <a href="https://galatians.vercel.app/#first_peter">
+             <Image 
+                                src="/previous.webp"
+                                alt="left"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+       </a>
+
+         <a href="#PayPalButton">
+            <Image 
+                                src="/upward_pointing.webp"
+                                alt="home"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         <a href="https://the-word-of-god.vercel.app/">
+           <Image 
+                                src="/next.webp"
+                                alt="right"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
+  
+                <div className={styles.iframe}>
+                     <iframe width="100%" height="315" src="https://www.youtube.com/embed/oFZknKPNvz8?si=KFesZgPBDm_hcn5E?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div> 
+ 
+      <div className={styles.grid6}>
+          <div className={styles.card}>
+            <h2 className={styles.chapter6}>Zephaniah Chpt I</h2>
+            <p>
+1 The word of the Lord that came to Zephaniah son of Cushi, the son of Gedaliah, the son of Amariah, the son of Hezekiah, during the reign of Josiah son of Amon king of Judah:
+
+Judgment on the Whole Earth in the Day of the Lord
+2 “I will sweep away everything
+    from the face of the earth,”
+declares the Lord.
+3 “I will sweep away both man and beast;
+    I will sweep away the birds in the sky
+    and the fish in the sea—
+    and the idols that cause the wicked to stumble.”[a]
+
+“When I destroy all mankind
+    on the face of the earth,”
+declares the Lord,
+4 “I will stretch out my hand against Judah
+    and against all who live in Jerusalem.
+I will destroy every remnant of Baal worship in this place,
+    the very names of the idolatrous priests—
+5 those who bow down on the roofs
+    to worship the starry host,
+those who bow down and swear by the Lord
+    and who also swear by Molek,[b]
+6 those who turn back from following the Lord
+    and neither seek the Lord nor inquire of him.”
+
+7 Be silent before the Sovereign Lord,
+    for the day of the Lord is near.
+The Lord has prepared a sacrifice;
+    he has consecrated those he has invited.
+
+8 “On the day of the Lord’s sacrifice
+    I will punish the officials
+    and the king’s sons
+and all those clad
+    in foreign clothes.
+9 On that day I will punish
+    all who avoid stepping on the threshold,[c]
+who fill the temple of their gods
+    with violence and deceit.
+
+10 “On that day,”
+    declares the Lord,
+“a cry will go up from the Fish Gate,
+    wailing from the New Quarter,
+    and a loud crash from the hills.
+11 Wail, you who live in the market district[d];
+    all your merchants will be wiped out,
+    all who trade with[e] silver will be destroyed.
+12 At that time I will search Jerusalem with lamps
+    and punish those who are complacent,
+    who are like wine left on its dregs,
+who think, ‘The Lord will do nothing,
+    either good or bad.’
+13 Their wealth will be plundered,
+    their houses demolished.
+Though they build houses,
+    they will not live in them;
+though they plant vineyards,
+    they will not drink the wine.”
+
+14 The great day of the Lord is near—
+    near and coming quickly.
+The cry on the day of the Lord is bitter;
+    the Mighty Warrior shouts his battle cry.
+15 That day will be a day of wrath—
+    a day of distress and anguish,
+        a day of trouble and ruin,
+    a day of darkness and gloom,
+        a day of clouds and blackness—
+16     a day of trumpet and battle cry
+against the fortified cities
+    and against the corner towers.
+
+17 “I will bring such distress on all people
+    that they will grope about like those who are blind,
+    because they have sinned against the Lord.
+Their blood will be poured out like dust
+    and their entrails like dung.
+18 Neither their silver nor their gold
+    will be able to save them
+    on the day of the Lord’s wrath.”
+
+In the fire of his jealousy
+    the whole earth will be consumed,
+for he will make a sudden end
+    of all who live on the earth.
+
+  
+                                  </p>
+              </div>
+
+          <div className={styles.card}>
+           <h2 className={styles.chapter6}>Zephaniah Chpt II</h2>
+            <p>
+Judah and Jerusalem Judged Along With the Nations
+Judah Summoned to Repent
+2 Gather together, gather yourselves together,
+    you shameful nation,
+2 before the decree takes effect
+    and that day passes like windblown chaff,
+before the Lord’s fierce anger
+    comes upon you,
+before the day of the Lord’s wrath
+    comes upon you.
+3 Seek the Lord, all you humble of the land,
+    you who do what he commands.
+Seek righteousness, seek humility;
+    perhaps you will be sheltered
+    on the day of the Lord’s anger.
+
+Philistia
+4 Gaza will be abandoned
+    and Ashkelon left in ruins.
+At midday Ashdod will be emptied
+    and Ekron uprooted.
+5 Woe to you who live by the sea,
+    you Kerethite people;
+the word of the Lord is against you,
+    Canaan, land of the Philistines.
+He says, “I will destroy you,
+    and none will be left.”
+6 The land by the sea will become pastures
+    having wells for shepherds
+    and pens for flocks.
+7 That land will belong
+    to the remnant of the people of Judah;
+    there they will find pasture.
+In the evening they will lie down
+    in the houses of Ashkelon.
+The Lord their God will care for them;
+    he will restore their fortunes.[a]
+
+Moab and Ammon
+8 “I have heard the insults of Moab
+    and the taunts of the Ammonites,
+who insulted my people
+    and made threats against their land.
+9 Therefore, as surely as I live,”
+    declares the Lord Almighty,
+    the God of Israel,
+“surely Moab will become like Sodom,
+    the Ammonites like Gomorrah—
+a place of weeds and salt pits,
+    a wasteland forever.
+The remnant of my people will plunder them;
+    the survivors of my nation will inherit their land.”
+
+10 This is what they will get in return for their pride,
+    for insulting and mocking
+    the people of the Lord Almighty.
+11 The Lord will be awesome to them
+    when he destroys all the gods of the earth.
+Distant nations will bow down to him,
+    all of them in their own lands.
+
+Cush
+12 “You Cushites,[b] too,
+    will be slain by my sword.”
+
+Assyria
+13 He will stretch out his hand against the north
+    and destroy Assyria,
+leaving Nineveh utterly desolate
+    and dry as the desert.
+14 Flocks and herds will lie down there,
+    creatures of every kind.
+The desert owl and the screech owl
+    will roost on her columns.
+Their hooting will echo through the windows,
+    rubble will fill the doorways,
+    the beams of cedar will be exposed.
+15 This is the city of revelry
+    that lived in safety.
+She said to herself,
+    “I am the one! And there is none besides me.”
+What a ruin she has become,
+    a lair for wild beasts!
+All who pass by her scoff
+    and shake their fists.
+
+
+  
+            </p>
+          </div>
+
+ <div className={styles.card}>
+           <h2 className={styles.chapter6}>Zephaniah Chpt III</h2>
+            <p>
+
+Jerusalem
+3 Woe to the city of oppressors,
+    rebellious and defiled!
+2 She obeys no one,
+    she accepts no correction.
+She does not trust in the Lord,
+    she does not draw near to her God.
+3 Her officials within her
+    are roaring lions;
+her rulers are evening wolves,
+    who leave nothing for the morning.
+4 Her prophets are unprincipled;
+    they are treacherous people.
+Her priests profane the sanctuary
+    and do violence to the law.
+5 The Lord within her is righteous;
+    he does no wrong.
+Morning by morning he dispenses his justice,
+    and every new day he does not fail,
+    yet the unrighteous know no shame.
+
+Jerusalem Remains Unrepentant
+6 “I have destroyed nations;
+    their strongholds are demolished.
+I have left their streets deserted,
+    with no one passing through.
+Their cities are laid waste;
+    they are deserted and empty.
+7 Of Jerusalem I thought,
+    ‘Surely you will fear me
+    and accept correction!’
+Then her place of refuge[a] would not be destroyed,
+    nor all my punishments come upon[b] her.
+But they were still eager
+    to act corruptly in all they did.
+8 Therefore wait for me,”
+    declares the Lord,
+    “for the day I will stand up to testify.[c]
+I have decided to assemble the nations,
+    to gather the kingdoms
+and to pour out my wrath on them—
+    all my fierce anger.
+The whole world will be consumed
+    by the fire of my jealous anger.
+
+Restoration of Israel’s Remnant
+9 “Then I will purify the lips of the peoples,
+    that all of them may call on the name of the Lord
+    and serve him shoulder to shoulder.
+10 From beyond the rivers of Cush[d]
+    my worshipers, my scattered people,
+    will bring me offerings.
+11 On that day you, Jerusalem, will not be put to shame
+    for all the wrongs you have done to me,
+because I will remove from you
+    your arrogant boasters.
+Never again will you be haughty
+    on my holy hill.
+12 But I will leave within you
+    the meek and humble.
+The remnant of Israel
+    will trust in the name of the Lord.
+13 They will do no wrong;
+    they will tell no lies.
+A deceitful tongue
+    will not be found in their mouths.
+They will eat and lie down
+    and no one will make them afraid.”
+
+14 Sing, Daughter Zion;
+    shout aloud, Israel!
+Be glad and rejoice with all your heart,
+    Daughter Jerusalem!
+15 The Lord has taken away your punishment,
+    he has turned back your enemy.
+The Lord, the King of Israel, is with you;
+    never again will you fear any harm.
+16 On that day
+    they will say to Jerusalem,
+“Do not fear, Zion;
+    do not let your hands hang limp.
+17 The Lord your God is with you,
+    the Mighty Warrior who saves.
+He will take great delight in you;
+    in his love he will no longer rebuke you,
+    but will rejoice over you with singing.”
+
+18 “I will remove from you
+    all who mourn over the loss of your appointed festivals,
+    which is a burden and reproach for you.
+19 At that time I will deal
+    with all who oppressed you.
+I will rescue the lame;
+    I will gather the exiles.
+I will give them praise and honor
+    in every land where they have suffered shame.
+20 At that time I will gather you;
+    at that time I will bring you home.
+I will give you honor and praise
+    among all the peoples of the earth
+when I restore your fortunes[e]
+    before your very eyes,”
+says the Lord.  
+            </p>
+          </div>
+
+  
+
+                          
+                            </div>
+                      </div>
+              
+      </main>
+
+
+
+
+
+
+  
+
  <main id="haggai" className={styles.main}>
       
        
@@ -6625,7 +7003,7 @@ he will bring down his anger with fury,
                               />
         </div>
 <p className={styles.parables}>
-      Haggai is in the develpopment stages....                                
+      The word of the Lord comes to Haggai to instruct Joshua & Zerubbabel to rebuild the temple                           
 </p>
                       <div className={styles.nav3}>           
       <a href="https://galatians.vercel.app/#first_peter">
