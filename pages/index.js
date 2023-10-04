321113970b65bd887d91f449da419b4c6974be5c
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-10-04 17:13:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,323 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+<main id="zephaniah" className={styles.main}>
+      
+       
+       
+        <div className={styles.hero}>
+           <h2 className={styles.holy}>THE WORD</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-holy-bible.vercel.app/" target="_blank">OF GOD</a>
+                </h1>
+        
+        <h1 className={styles.title}>
+          ZEPHANIAH
+        </h1>
+         <div  className={styles.image}>
+       <Image 
+                                src="/habakkuk.png"
+                                alt="haggai image"
+                                height={900}
+                                width={2000}
+                                priority
+                              />
+        </div>
+<p className={styles.parables}>
+The prophecy Habakkuk the prophet received... How long, Lord, must I call for help,
+    but you do not listen? </p>
+                      <div className={styles.nav7}>           
+      <a href="https://galatians.vercel.app/#first_peter">
+             <Image 
+                                src="/previous.webp"
+                                alt="left"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+       </a>
+
+         <a href="#PayPalButton">
+            <Image 
+                                src="/upward_pointing.webp"
+                                alt="home"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         <a href="https://the-word-of-god.vercel.app/">
+           <Image 
+                                src="/next.webp"
+                                alt="right"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
+  
+                <div className={styles.iframe}>
+                     <iframe width="100%" height="315" src="https://www.youtube.com/embed/OPMaRqGJPUU?si=NDgBIQbY-IiPBk1K?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div> 
+ 
+      <div className={styles.grid7}>
+          <div className={styles.card}>
+            <h2 className={styles.chapter7}>Habakkuk Chpt I</h2>
+            <p>
+1 The prophecy that Habakkuk the prophet received.
+
+Habakkuk’s Complaint
+2 How long, Lord, must I call for help,
+    but you do not listen?
+Or cry out to you, “Violence!”
+    but you do not save?
+3 Why do you make me look at injustice?
+    Why do you tolerate wrongdoing?
+Destruction and violence are before me;
+    there is strife, and conflict abounds.
+4 Therefore the law is paralyzed,
+    and justice never prevails.
+The wicked hem in the righteous,
+    so that justice is perverted.
+
+The Lord’s Answer
+5 “Look at the nations and watch—
+    and be utterly amazed.
+For I am going to do something in your days
+    that you would not believe,
+    even if you were told.
+6 I am raising up the Babylonians,[a]
+    that ruthless and impetuous people,
+who sweep across the whole earth
+    to seize dwellings not their own.
+7 They are a feared and dreaded people;
+    they are a law to themselves
+    and promote their own honor.
+8 Their horses are swifter than leopards,
+    fiercer than wolves at dusk.
+Their cavalry gallops headlong;
+    their horsemen come from afar.
+They fly like an eagle swooping to devour;
+9     they all come intent on violence.
+Their hordes[b] advance like a desert wind
+    and gather prisoners like sand.
+10 They mock kings
+    and scoff at rulers.
+They laugh at all fortified cities;
+    by building earthen ramps they capture them.
+11 Then they sweep past like the wind and go on—
+    guilty people, whose own strength is their god.”
+
+Habakkuk’s Second Complaint
+12 Lord, are you not from everlasting?
+    My God, my Holy One, you[c] will never die.
+You, Lord, have appointed them to execute judgment;
+    you, my Rock, have ordained them to punish.
+13 Your eyes are too pure to look on evil;
+    you cannot tolerate wrongdoing.
+Why then do you tolerate the treacherous?
+    Why are you silent while the wicked
+    swallow up those more righteous than themselves?
+14 You have made people like the fish in the sea,
+    like the sea creatures that have no ruler.
+15 The wicked foe pulls all of them up with hooks,
+    he catches them in his net,
+he gathers them up in his dragnet;
+    and so he rejoices and is glad.
+16 Therefore he sacrifices to his net
+    and burns incense to his dragnet,
+for by his net he lives in luxury
+    and enjoys the choicest food.
+17 Is he to keep on emptying his net,
+    destroying nations without mercy?  
+                                  </p>
+              </div>
+
+          <div className={styles.card}>
+           <h2 className={styles.chapter7}>Habakkuk Chpt II</h2>
+            <p>
+
+2 I will stand at my watch
+    and station myself on the ramparts;
+I will look to see what he will say to me,
+    and what answer I am to give to this complaint.[a]
+
+The Lord’s Answer
+2 Then the Lord replied:
+
+“Write down the revelation
+    and make it plain on tablets
+    so that a herald[b] may run with it.
+3 For the revelation awaits an appointed time;
+    it speaks of the end
+    and will not prove false.
+Though it linger, wait for it;
+    it[c] will certainly come
+    and will not delay.
+
+4 “See, the enemy is puffed up;
+    his desires are not upright—
+    but the righteous person will live by his faithfulness[d]—
+5 indeed, wine betrays him;
+    he is arrogant and never at rest.
+Because he is as greedy as the grave
+    and like death is never satisfied,
+he gathers to himself all the nations
+    and takes captive all the peoples.
+
+6 “Will not all of them taunt him with ridicule and scorn, saying,
+
+“‘Woe to him who piles up stolen goods
+    and makes himself wealthy by extortion!
+    How long must this go on?’
+7 Will not your creditors suddenly arise?
+    Will they not wake up and make you tremble?
+    Then you will become their prey.
+8 Because you have plundered many nations,
+    the peoples who are left will plunder you.
+For you have shed human blood;
+    you have destroyed lands and cities and everyone in them.
+
+9 “Woe to him who builds his house by unjust gain,
+    setting his nest on high
+    to escape the clutches of ruin!
+10 You have plotted the ruin of many peoples,
+    shaming your own house and forfeiting your life.
+11 The stones of the wall will cry out,
+    and the beams of the woodwork will echo it.
+
+12 “Woe to him who builds a city with bloodshed
+    and establishes a town by injustice!
+13 Has not the Lord Almighty determined
+    that the people’s labor is only fuel for the fire,
+    that the nations exhaust themselves for nothing?
+14 For the earth will be filled with the knowledge of the glory of the Lord
+    as the waters cover the sea.
+
+15 “Woe to him who gives drink to his neighbors,
+    pouring it from the wineskin till they are drunk,
+    so that he can gaze on their naked bodies!
+16 You will be filled with shame instead of glory.
+    Now it is your turn! Drink and let your nakedness be exposed[e]!
+The cup from the Lord’s right hand is coming around to you,
+    and disgrace will cover your glory.
+17 The violence you have done to Lebanon will overwhelm you,
+    and your destruction of animals will terrify you.
+For you have shed human blood;
+    you have destroyed lands and cities and everyone in them.
+
+18 “Of what value is an idol carved by a craftsman?
+    Or an image that teaches lies?
+For the one who makes it trusts in his own creation;
+    he makes idols that cannot speak.
+19 Woe to him who says to wood, ‘Come to life!’
+    Or to lifeless stone, ‘Wake up!’
+Can it give guidance?
+    It is covered with gold and silver;
+    there is no breath in it.”
+
+20 The Lord is in his holy temple;
+    let all the earth be silent before him.  
+            </p>
+          </div>
+
+ <div className={styles.card}>
+           <h2 className={styles.chapter7}>Habakkuk Chpt III</h2>
+            <p>
+Habakkuk’s Prayer
+3 A prayer of Habakkuk the prophet. On shigionoth.[a]
+
+2 Lord, I have heard of your fame;
+    I stand in awe of your deeds, Lord.
+Repeat them in our day,
+    in our time make them known;
+    in wrath remember mercy.
+
+3 God came from Teman,
+    the Holy One from Mount Paran.[b]
+His glory covered the heavens
+    and his praise filled the earth.
+4 His splendor was like the sunrise;
+    rays flashed from his hand,
+    where his power was hidden.
+5 Plague went before him;
+    pestilence followed his steps.
+6 He stood, and shook the earth;
+    he looked, and made the nations tremble.
+The ancient mountains crumbled
+    and the age-old hills collapsed—
+    but he marches on forever.
+7 I saw the tents of Cushan in distress,
+    the dwellings of Midian in anguish.
+
+8 Were you angry with the rivers, Lord?
+    Was your wrath against the streams?
+Did you rage against the sea
+    when you rode your horses
+    and your chariots to victory?
+9 You uncovered your bow,
+    you called for many arrows.
+You split the earth with rivers;
+10     the mountains saw you and writhed.
+Torrents of water swept by;
+    the deep roared
+    and lifted its waves on high.
+
+11 Sun and moon stood still in the heavens
+    at the glint of your flying arrows,
+    at the lightning of your flashing spear.
+12 In wrath you strode through the earth
+    and in anger you threshed the nations.
+13 You came out to deliver your people,
+    to save your anointed one.
+You crushed the leader of the land of wickedness,
+    you stripped him from head to foot.
+14 With his own spear you pierced his head
+    when his warriors stormed out to scatter us,
+gloating as though about to devour
+    the wretched who were in hiding.
+15 You trampled the sea with your horses,
+    churning the great waters.
+
+16 I heard and my heart pounded,
+    my lips quivered at the sound;
+decay crept into my bones,
+    and my legs trembled.
+Yet I will wait patiently for the day of calamity
+    to come on the nation invading us.
+17 Though the fig tree does not bud
+    and there are no grapes on the vines,
+though the olive crop fails
+    and the fields produce no food,
+though there are no sheep in the pen
+    and no cattle in the stalls,
+18 yet I will rejoice in the Lord,
+    I will be joyful in God my Savior.
+
+19 The Sovereign Lord is my strength;
+    he makes my feet like the feet of a deer,
+    he enables me to tread on the heights.
+
+For the director of music. On my stringed instruments.
+            </p>
+          </div>
+
+  
+
+                          
+                            </div>
+                      </div>
+              
+      </main>
+
+
+
+
+    
 
 <main id="zephaniah" className={styles.main}>
       
