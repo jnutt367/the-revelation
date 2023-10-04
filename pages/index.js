f0a32aa33ae57de6713cd652c35d9919f9cec1d9
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-10-04 17:56:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,318 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
+
+
+
+<main id="nahum" className={styles.main}>
+      
+       
+       
+        <div className={styles.hero}>
+           <h2 className={styles.holy}>THE WORD</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-holy-bible.vercel.app/" target="_blank">OF GOD</a>
+                </h1>
+        
+        <h1 className={styles.title}>
+          NAHUM
+        </h1>
+         <div  className={styles.image}>
+       <Image 
+                                src="/nahum.png"
+                                alt="haggai image"
+                                height={900}
+                                width={2000}
+                                priority
+                              />
+        </div>
+<p className={styles.parables}>
+An oracle concerning Nineveh. The vision of Nehum the Eloshite. "The Lord is a jealous & avenging God."
+    but you do not listen? </p>
+                      <div className={styles.nav8}>           
+      <a href="https://the-revelation.vercel.app/#micah">
+             <Image 
+                                src="/previous.webp"
+                                alt="left"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+       </a>
+
+         <a href="#PayPalButton">
+            <Image 
+                                src="/upward_pointing.webp"
+                                alt="home"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         <a href="https://the-revelation.vercel.app/#habakkuk">
+           <Image 
+                                src="/next.webp"
+                                alt="right"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
+  
+                <div className={styles.iframe}>
+                     <iframe width="100%" height="315" src="https://www.youtube.com/embed/Y30DanA5EhU?si=PbZupHFhEoyCosFw?autoplay&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div> 
+ 
+      <div className={styles.grid8}>
+          <div className={styles.card}>
+            <h2 className={styles.chapter8}>Nahum Chpt I</h2>
+            <p>
+
+1 A prophecy concerning Nineveh. The book of the vision of Nahum the Elkoshite.
+
+The Lord’s Anger Against Nineveh
+2 The Lord is a jealous and avenging God;
+    the Lord takes vengeance and is filled with wrath.
+The Lord takes vengeance on his foes
+    and vents his wrath against his enemies.
+3 The Lord is slow to anger but great in power;
+    the Lord will not leave the guilty unpunished.
+His way is in the whirlwind and the storm,
+    and clouds are the dust of his feet.
+4 He rebukes the sea and dries it up;
+    he makes all the rivers run dry.
+Bashan and Carmel wither
+    and the blossoms of Lebanon fade.
+5 The mountains quake before him
+    and the hills melt away.
+The earth trembles at his presence,
+    the world and all who live in it.
+6 Who can withstand his indignation?
+    Who can endure his fierce anger?
+His wrath is poured out like fire;
+    the rocks are shattered before him.
+
+7 The Lord is good,
+    a refuge in times of trouble.
+He cares for those who trust in him,
+8     but with an overwhelming flood
+he will make an end of Nineveh;
+    he will pursue his foes into the realm of darkness.
+
+9 Whatever they plot against the Lord
+    he will bring[a] to an end;
+    trouble will not come a second time.
+10 They will be entangled among thorns
+    and drunk from their wine;
+    they will be consumed like dry stubble.[b]
+11 From you, Nineveh, has one come forth
+    who plots evil against the Lord
+    and devises wicked plans.
+
+12 This is what the Lord says:
+
+“Although they have allies and are numerous,
+    they will be destroyed and pass away.
+Although I have afflicted you, Judah,
+    I will afflict you no more.
+13 Now I will break their yoke from your neck
+    and tear your shackles away.”
+
+14 The Lord has given a command concerning you, Nineveh:
+    “You will have no descendants to bear your name.
+I will destroy the images and idols
+    that are in the temple of your gods.
+I will prepare your grave,
+    for you are vile.”
+
+15 Look, there on the mountains,
+    the feet of one who brings good news,
+    who proclaims peace!
+Celebrate your festivals, Judah,
+    and fulfill your vows.
+No more will the wicked invade you;
+    they will be completely destroyed.[c]
+                                  
+                                  </p>
+              </div>
+
+          <div className={styles.card}>
+           <h2 className={styles.chapter8}>Nahum Chpt II</h2>
+            <p>
+
+Nineveh to Fall
+2 [a]An attacker advances against you, Nineveh.
+    Guard the fortress,
+    watch the road,
+    brace yourselves,
+    marshal all your strength!
+
+2 The Lord will restore the splendor of Jacob
+    like the splendor of Israel,
+though destroyers have laid them waste
+    and have ruined their vines.
+
+3 The shields of the soldiers are red;
+    the warriors are clad in scarlet.
+The metal on the chariots flashes
+    on the day they are made ready;
+    the spears of juniper are brandished.[b]
+4 The chariots storm through the streets,
+    rushing back and forth through the squares.
+They look like flaming torches;
+    they dart about like lightning.
+
+5 Nineveh summons her picked troops,
+    yet they stumble on their way.
+They dash to the city wall;
+    the protective shield is put in place.
+6 The river gates are thrown open
+    and the palace collapses.
+7 It is decreed[c] that Nineveh
+    be exiled and carried away.
+Her female slaves moan like doves
+    and beat on their breasts.
+8 Nineveh is like a pool
+    whose water is draining away.
+“Stop! Stop!” they cry,
+    but no one turns back.
+9 Plunder the silver!
+    Plunder the gold!
+The supply is endless,
+    the wealth from all its treasures!
+10 She is pillaged, plundered, stripped!
+    Hearts melt, knees give way,
+    bodies tremble, every face grows pale.
+
+11 Where now is the lions’ den,
+    the place where they fed their young,
+where the lion and lioness went,
+    and the cubs, with nothing to fear?
+12 The lion killed enough for his cubs
+    and strangled the prey for his mate,
+filling his lairs with the kill
+    and his dens with the prey.
+
+13 “I am against you,”
+    declares the Lord Almighty.
+“I will burn up your chariots in smoke,
+    and the sword will devour your young lions.
+    I will leave you no prey on the earth.
+The voices of your messengers
+    will no longer be heard.”            </p>
+          </div>
+
+ <div className={styles.card}>
+           <h2 className={styles.chapter8}>Nahum Chpt III</h2>
+            <p>
+
+Woe to Nineveh
+3 Woe to the city of blood,
+    full of lies,
+full of plunder,
+    never without victims!
+2 The crack of whips,
+    the clatter of wheels,
+galloping horses
+    and jolting chariots!
+3 Charging cavalry,
+    flashing swords
+    and glittering spears!
+Many casualties,
+    piles of dead,
+bodies without number,
+    people stumbling over the corpses—
+4 all because of the wanton lust of a prostitute,
+    alluring, the mistress of sorceries,
+who enslaved nations by her prostitution
+    and peoples by her witchcraft.
+
+5 “I am against you,” declares the Lord Almighty.
+    “I will lift your skirts over your face.
+I will show the nations your nakedness
+    and the kingdoms your shame.
+6 I will pelt you with filth,
+    I will treat you with contempt
+    and make you a spectacle.
+7 All who see you will flee from you and say,
+    ‘Nineveh is in ruins—who will mourn for her?’
+    Where can I find anyone to comfort you?”
+
+8 Are you better than Thebes,
+    situated on the Nile,
+    with water around her?
+The river was her defense,
+    the waters her wall.
+9 Cush[a] and Egypt were her boundless strength;
+    Put and Libya were among her allies.
+10 Yet she was taken captive
+    and went into exile.
+Her infants were dashed to pieces
+    at every street corner.
+Lots were cast for her nobles,
+    and all her great men were put in chains.
+11 You too will become drunk;
+    you will go into hiding
+    and seek refuge from the enemy.
+
+12 All your fortresses are like fig trees
+    with their first ripe fruit;
+when they are shaken,
+    the figs fall into the mouth of the eater.
+13 Look at your troops—
+    they are all weaklings.
+The gates of your land
+    are wide open to your enemies;
+    fire has consumed the bars of your gates.
+
+14 Draw water for the siege,
+    strengthen your defenses!
+Work the clay,
+    tread the mortar,
+    repair the brickwork!
+15 There the fire will consume you;
+    the sword will cut you down—
+    they will devour you like a swarm of locusts.
+Multiply like grasshoppers,
+    multiply like locusts!
+16 You have increased the number of your merchants
+    till they are more numerous than the stars in the sky,
+but like locusts they strip the land
+    and then fly away.
+17 Your guards are like locusts,
+    your officials like swarms of locusts
+    that settle in the walls on a cold day—
+but when the sun appears they fly away,
+    and no one knows where.
+
+18 King of Assyria, your shepherds[b] slumber;
+    your nobles lie down to rest.
+Your people are scattered on the mountains
+    with no one to gather them.
+19 Nothing can heal you;
+    your wound is fatal.
+All who hear the news about you
+    clap their hands at your fall,
+for who has not felt
+    your endless cruelty?
+                                  
+            </p>
+          </div>
+
+  
+
+                          
+                            </div>
+                      </div>
+              
+      </main>
+
+    
 <main id="habakkuk" className={styles.main}>
       
        
