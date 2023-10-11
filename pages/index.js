5fff7deea332664f91cb907a07f27c45e0420483
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-10-10 20:53:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>The old Testament NIV</title>
+        <title>The Prophets, Jeremiah - Malachi</title>
         <meta name="description" content="Ezekiel" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
