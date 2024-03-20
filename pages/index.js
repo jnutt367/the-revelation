4b3f26b2068d1d3599ba8bb2f56f112b9ce34aea
Jason Nutt | A TechNutt <jnutt367@gmail.com>
2024-03-20 11:19:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,11 @@ export default function Home() {
         <meta name="description" content="Ezekiel" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-<main id="jeremiah" className={styles.main}>
+ <div class="iframe1">
+    <iframe src="https://api.chatlab.com/aichat/iframe?apiKey=d5a4bf57-ad50-41c9-a2df-fc39065dceba&iFrameMode=true" width="50%" height="300px"></iframe>
+    </div>
+    
+    <main id="jeremiah" className={styles.main}>
       
       <div className={styles.hero}>
       
